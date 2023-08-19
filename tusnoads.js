92b60949c267dd9aedaa8d4307fedad894f76d52
riahohjjkjh <142058674+riahohjjkjh@users.noreply.github.com>
2023-08-19 23:39:36 +0200

    Create tusnoads.js
    noad script code java

---
+++ tusnoads.js
@@ -0,0 +1,60 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function() {
+document.write("<center>");
+document.write("<img style='display:none;' src='' width='0' height='0' border='0'><img style='display:none;' src='//web.archive.org/web/20220714032834/https://counter.yadro.ru/hit?t23.6;uhttp://www.elahmad.com/tv/Webtv/Arabian.htm' border='0' width='0' height='0'>");
+var elhmd_bookmarkTitle = elhmd_bookmarkTitle || document.title;
+var elhmd_bookmarkUrl = elhmd_bookmarkUrl || document.location;
+var elhmd_bookmarkPub = elhmd_bookmarkPub || document.domain || "unknow";
+var elhmd_imagesUrl = "//web.archive.org/web/20220714032834/https://www.elahmad.com/images/bookmark";
+var elhmd_encode = encodeURIComponent;
+document.write('<p style="margin:0"><a rel="nofollow" target="_parent" href="javascript:if ((navigator.appName == \'Microsoft Internet Explorer\') && (document.all)) {window.external.AddFavorite(\'' + elhmd_bookmarkUrl + '\', \'' + elhmd_bookmarkTitle + '\')} else if (window.sidebar) {window.sidebar.addPanel(\'' + elhmd_bookmarkTitle + '\', \'' + elhmd_bookmarkUrl + '\', \'\')} else {alert(\'Press CTRL-D (Netscape) or CTRL-T (Opera) to bookmark\')};"><img border="0" src="' + elhmd_imagesUrl + '/bookmark.gif" title="Bookmark &#x627;&#x636;&#x641;&#x646;&#x627; &#x644;&#x644;&#x645;&#x641;&#x636;&#x644;&#x629;" alt="Bookmark"  width="20" height="20" /></a>&nbsp;');
+document.write('<a rel="nofollow" target="_blank" href="//web.archive.org/web/20220714032834/https://digg.com/submit?phase=2&url=' + elhmd_encode(elhmd_bookmarkUrl) + '&amp;title=' + elhmd_encode(elhmd_bookmarkTitle) + '"><img border="0" src="' + elhmd_imagesUrl + '/digg.gif" title="Digg" alt="Digg" width="20" height="20"  /></a>&nbsp;');
+document.write('<a rel="nofollow" target="_blank" href="//web.archive.org/web/20220714032834/https://www.delicious.com/save?url=' + elhmd_encode(elhmd_bookmarkUrl) + '&amp;title=' + elhmd_encode(elhmd_bookmarkTitle) + '&amp;notes=' + elhmd_encode(elhmd_bookmarkPub) + '"><img border="0" src="' + elhmd_imagesUrl + '/delicious.gif" title="Delicious" alt="Delicious" width="20" height="20" /></a>&nbsp;');
+document.write('<a rel="nofollow" target="_blank" href="http://web.archive.org/web/20220714032834/https://www.google.com/bookmarks/mark?op=add&amp;bkmk=' + elhmd_encode(elhmd_bookmarkUrl) + '&title=' + elhmd_encode(elhmd_bookmarkTitle) + '&annotation=' + elhmd_encode(elhmd_bookmarkPub) + '"><img border="0" src="' + elhmd_imagesUrl + '/google.gif" title="Google" alt="Google" width="20" height="20"  /></a>&nbsp;');
+document.write('<a rel="nofollow" target="_blank" href="//web.archive.org/web/20220714032834/https://twitter.com/share?url=' + elhmd_encode(elhmd_bookmarkUrl) + '&via=elahmad_com&amp;text=' + elhmd_encode(elhmd_bookmarkTitle) + '&amp;p=' + elhmd_encode(elhmd_bookmarkPub) + '"><img border="0" src="' + elhmd_imagesUrl + '/twitter.gif" title="Twitter" alt="Twitter" width="20" height="20" /></a>&nbsp;');
+document.write('<a rel="nofollow" target="_blank" title="Send to facebook &#x627;&#x636;&#x641; &#x647;&#x630;&#x647; &#x627;&#x644;&#x635;&#x641;&#x62d;&#x629; &#x627;&#x644;&#x649; &#x627;&#x644;&#x641;&#x64a;&#x633; &#x628;&#x648;&#x643; "  href="//web.archive.org/web/20220714032834/https://www.facebook.com/sharer.php?u=' + elhmd_bookmarkUrl + '&t=' + elhmd_bookmarkTitle + '" ><img border="0" src="' + elhmd_imagesUrl + '/facebook.gif" width="20" height="20"></a>');
+//document.write('&nbsp;<g:plus action="share" count="false" href="' + elhmd_encode(elhmd_bookmarkUrl) + '" onendinteraction="closeBubble" onstartinteraction="openBubble"></g:plus>&nbsp;<g:plusone size="medium" count="false" href="' + elhmd_encode(elhmd_bookmarkUrl) + '"></g:plusone></p>');
+document.write('<p style="margin:0"><iframe src="//web.archive.org/web/20220714032834/https://www.facebook.com/plugins/like.php?api_key=1400009843568799&href=' + elhmd_encode(elhmd_bookmarkUrl) + '&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&share=true&" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:125px; height:21px;" allowTransparency="true"></iframe>&nbsp;<iframe src="//web.archive.org/web/20220714032834/https://www.facebook.com/plugins/like.php?api_key=1400009843568799&href=http%3A%2F%2Fwww.elahmad.com&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe></p>');
+EXs=screen;EXw=EXs.width;navigator.appName!="Netscape"?EXb=EXs.colorDepth:EXb=EXs.pixelDepth;navigator.javaEnabled()==1?EXjv="y":EXjv="n";EXd=document;EXw?"":EXw="na";EXb?"":EXb="na";EXd.write("<img src=//e2.extreme-dm.com","/s11.g?login=elahmad&amp;","jv="+EXjv+"&amp;j=y&amp;srw="+EXw+"&amp;srb="+EXb+"&amp;","l="+escape(parent.document.referrer)+" style='display:none;' height=1 width=1>");
+
+//try {Noowho_ref = top.document.referrer;}catch(e) {Noowho_ref = document.referrer;} Noowho_ref.replace(/[<>"]/g, '').replace(/&/g, '%26');
+//document.write('<img src="//web.archive.org/web/20220714032834/https://www.noowho.com/image.php?site=23694493&ref='+Noowho_ref+'" style="display:none;" />');
+//google
+(function(d,e,j,h,f,c,b){d.GoogleAnalyticsObject=f;d[f]=d[f]||function(){(d[f].q=d[f].q||[]).push(arguments)},d[f].l=1*new Date();c=e.createElement(j),b=e.getElementsByTagName(j)[0];c.async=1;c.src=h;b.parentNode.insertBefore(c,b)})(window,document,"script","http://web.archive.org/web/20220714032834/https://www.google-analytics.com/analytics.js","ga");ga("create","UA-6843551-1","auto");ga("send","pageview");
+//google plus
+ //(function(){var d=document.createElement("script");d.type="text/javascript";d.async=true;d.src="//web.archive.org/web/20220714032834/https://plus.google.com/js/client:plusone.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(d,c)})(); 
+document.write("</center>");
+})();
+
+}
+/*
+     FILE ARCHIVED ON 03:28:34 Jul 14, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:52:10 Jul 17, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 123.813
+  exclusion.robots: 0.118
+  exclusion.robots.policy: 0.109
+  cdx.remote: 0.052
+  esindex: 0.009
+  LoadShardBlock: 98.452 (3)
+  PetaboxLoader3.datanode: 62.979 (4)
+  load_resource: 100.488
+  PetaboxLoader3.resolve: 28.309
+*/
